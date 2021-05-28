490c9ac569ca5eed987de363e1a0d8136b469f55
junyi <collau@hotmail.com>
2021-05-28 22:32:38 +0800

    7. Setup Data Binding View and Show Next Question

--- app/src/main/java/com/fishnco/trivia/MainActivity.java
+++ app/src/main/java/com/fishnco/trivia/MainActivity.java
@@ -1,10 +1,14 @@
 package com.fishnco.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import android.os.Bundle;
+import android.view.View;
 
 import com.fishnco.trivia.data.AnswerListAsyncResponse;
 import com.fishnco.trivia.data.Repository;
+import com.fishnco.trivia.databinding.ActivityMainBinding;
 import com.fishnco.trivia.model.Question;
 
 import java.util.ArrayList;
@@ -13,12 +17,17 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
+    List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        /*
         Repository repository = new Repository();
         List<Question> questionList = repository.getQuestions(new AnswerListAsyncResponse() {
             @Override
@@ -27,5 +36,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        */
+
+        questionList = new Repository().getQuestions(new AnswerListAsyncResponse() {
+            @Override
+            public void processFinished(ArrayList<Question> questionArrayList) {
+                binding.textViewQuestion.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+            }
+        });
+
+        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentQuestionIndex = (currentQuestionIndex + 1 ) % questionList.size();
+                updateQuestion();
+            }
+        });
+
+        binding.buttonTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.buttonFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        binding.textViewQuestion.setText(question);
     }
 }
